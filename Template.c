7590d60718fff0fc3dd74636d20067034d9cb471
Eyder A. Concha <eyderacm@gmail.com>
2019-01-23 08:25:30 -0600

    add my name

--- Template.c
+++ Template.c
@@ -1,5 +1,5 @@
 /*
-Autor:Edgar Cambranes 26/Enero/17
+Autor:Eyder Concha 26/Enero/17
 Entradas:
 Salidas:
 Procedimiento general:
@@ -10,7 +10,8 @@ Procedimiento general:
 int main(int argc, char *argv[]) {
 	/*Entradas*/
 	
-	/*Procedimiento*/
+	/*Procedimiento*/
+
 	/*Salidas*/
 	return 0;
 }
