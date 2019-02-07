293ad817d3c233a4629894a39df3f3154f9d6ce8
JimmyOjeda <46965072+JimmyOjeda@users.noreply.github.com>
2019-02-07 15:54:13 -0600

    Update 25.c

--- Ejercicios/Ejercicio 25/C/25.c
+++ Ejercicios/Ejercicio 25/C/25.c
@@ -1,3 +1,19 @@
+Ejercicio 25 C
+Unidad 2 - {Programación Estructurada}
+Equipo 1
+Autor: Jimmy Nathan Ojeda Arana
+Fecha: 3 de Febrero 2019
+Entradas: Una letra perteneciente al código ASCII
+Salidas: El código decimal en ASCII correspondiente a la letra introducida
+---------------------------------------
+Instrucción:
+Escribir un programa para mostrar los códigos ASCII de las letras mayúsculas y minúsculas.
+---------------------------------------
+Procedimiento General:
+---------------------------------------
+1.-Se solicita al usuario ingresar una letra
+2.-Se imprime la misma letra en tipo entero
+---------------------------------------
 #include <stdio.h>
 
 int main()
